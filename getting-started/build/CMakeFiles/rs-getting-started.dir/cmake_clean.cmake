812e8a794e4d84de2fae97af91a7b7b377e953c8
Reza Abiri <reza.abiri2020@gmail.com>
2021-04-22 18:43:32 -0700

    Initial running test of camera

---
+++ getting-started/build/CMakeFiles/rs-getting-started.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rs-getting-started.dir/rs-getting-started.cpp.o"
+  "rs-getting-started.pdb"
+  "rs-getting-started"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rs-getting-started.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
